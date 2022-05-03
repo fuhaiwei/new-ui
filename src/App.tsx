638e659b7230748e56254b75b60bcb25478ce655
fuhaiwei <fuhaiwei@126.com>
2022-05-03 12:27:29 +0800

    feat: add package react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,19 @@
+import Demo from '#C/demo/Demo'
+import Home from '#C/home/Home'
+import { NavLink, Route, Routes } from 'react-router-dom'
 import './App.css'
-import { Counter } from './features/counter/Counter'
-import logo from './logo.svg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <div className="linked">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/demo">Demo</NavLink>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/demo" element={<Demo />} />
+      </Routes>
     </div>
   )
 }
