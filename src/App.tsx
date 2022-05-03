a1683270bc4a0b25ed57af23d152c37c0c38ee5a
fuhaiwei <fuhaiwei@126.com>
2022-05-03 16:20:27 +0800

    feat: Add github outlink and icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useNav } from '#A/hooks'
 import Demo from '#C/demo/Demo'
 import Home from '#C/home/Home'
+import { GithubOutlined, HomeOutlined, PlayCircleOutlined } from '@ant-design/icons'
 import { Layout, Menu } from 'antd'
 import { Content, Header } from 'antd/lib/layout/layout'
 import { ItemType } from 'antd/lib/menu/hooks/useItems'
@@ -9,14 +10,24 @@ import { Route, Routes } from 'react-router-dom'
 import './App.scss'
 
 const items: ItemType[] = [
-  { key: '/', label: 'Home' },
-  { key: '/demo', label: 'Demo' },
+  { label: 'Home', icon: <HomeOutlined />, key: '/' },
+  { label: 'Demo', icon: <PlayCircleOutlined />, key: '/demo' },
+  {
+    label: 'Source',
+    icon: <GithubOutlined />,
+    children: [
+      { label: 'Github - UI', key: 'https://github.com/fuhaiwei/new-ui' },
+      { label: 'Github - Server', key: 'https://github.com/mingzuozhibi/mzzb-server' },
+      { label: 'Github - Spider', key: 'https://github.com/mingzuozhibi/mzzb-spider' },
+      { label: 'Github - Admin', key: 'https://github.com/mingzuozhibi/mzzb-admin' },
+    ],
+  } as any,
 ]
 
 function App() {
   const { pathname, navigate } = useNav()
   const onClick = ({ key, domEvent }: MenuInfo) => {
-    if (domEvent.ctrlKey) {
+    if (key.startsWith('http') || domEvent.ctrlKey) {
       window.open(key)
     } else {
       navigate(key)
