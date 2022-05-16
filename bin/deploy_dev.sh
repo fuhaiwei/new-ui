f0b8c08c8f6fb921b0fe51dd8b713de298088947
fuhaiwei <fuhaiwei@126.com>
2022-05-16 09:11:35 +0800

    chore: Add deploy_dev.sh

---
+++ bin/deploy_dev.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+# 环境准备
+DirName=$(dirname "$0")
+HomeDir=$(realpath "$DirName"/..)
+cd "$HomeDir" || exit
+
+# 构建部署
+if [[ $1 != '-q' ]]; then
+    yarn build
+fi
+
+ssh q "rm -rf nginx/www/mzzb-ui-dev; mkdir -p nginx/www/mzzb-ui-dev;"
+tar -czf - -C build . | ssh q "tar -xzpvf - -C nginx/www/mzzb-ui-dev"
